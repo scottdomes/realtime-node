7f24496d33503b614101faa74e81f9dd467948d5
scottdomes <scott@musefind.com>
2019-02-05 16:39:03 -0800

    Basic frontend

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = { messages: [], newMessage: '' }
+
+  componentDidMount() {
+    this.loadMessages()
+  }
+
+  handleNewMessageChange = e => {
+    this.setState({ newMessage: e.target.value })
+  }
+
+  loadMessages() {
+   fetch('http://localhost:8080/messages')
+      .then(res => res.json())
+      .then(data => {
+        this.setState({ messages: data.messages})
+      })
+  }
+
+  sendMessage = () => {
+    fetch('http://localhost:8080/messages', { 
+      method: 'POST', 
+      body: {
+        message: this.state.newMessage
+      }
+    }).then(() => {
+      this.loadMessages()
+    })
+  }
+
   render() {
+    const { messages, newMessage } = this.state
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        {
+          messages.map(msg => <p key={msg.id}>{msg.text}</p>)
+        }
+        <textarea 
+          value={newMessage} 
+          onChange={this.handleNewMessageChange} 
+        />
+        <button onClick={this.sendMessage}>Send</button>
       </div>
     );
   }
