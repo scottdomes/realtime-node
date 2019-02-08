8fb4e03bbf2af74252dcd02758b1b1e0fa66dc93
scottdomes <scott@musefind.com>
2019-02-08 11:15:05 -0800

    Class based approach

--- server/index.js
+++ server/index.js
@@ -1,40 +1,21 @@
 import express from 'express';
-import { MongoClient } from 'mongodb'
-import bodyParser from 'body-parser'
-import cors from 'cors'
-import { createServer } from 'http'
-import socket from 'socket.io'
-import routes from './routes'
+import { MongoClient } from 'mongodb';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import { createServer } from 'http';
+import setUpSockets from './sockets';
 
 const app = express();
 app.use(bodyParser.json());
-app.use(cors())
+app.use(cors());
 const server = createServer(app);
 
+MongoClient.connect('mongodb://localhost/realtime', (err, database) => {
+  if (err) return console.log(err);
+  const db = database.db('realtime');
+  setUpSockets(server, db);
 
-
-MongoClient.connect(
-  'mongodb://localhost/realtime',
-  (err, database) => {
-    if (err) return console.log(err);
-    const db = database.db('realtime');
-    routes(app, db)
-    const io = socket(server)
-
-    io.on('connection', async (client) => {
-      console.log('connection!')
-      const messages = await db.collection('messages').find().toArray()
-      client.emit('messages', messages)
-      client.on('newMessage', message => { 
-        db.collection('messages').insert({ text: message }, async (err, result) => {
-          io.emit('messages', await db.collection('messages').find().toArray())
-        });
-      });
-      client.on('disconnect', () => { console.log('disconnect!') });
-    });
-
-    server.listen(8080, () => {
-      console.log('Listening at port', server.address().port);
-    });
-  }
-);
+  server.listen(8080, () => {
+    console.log('Listening at port', server.address().port);
+  });
+});
