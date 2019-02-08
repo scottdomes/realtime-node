9446d53c3674b1621fbdfe046be008d0bad074a8
scottdomes <scott@musefind.com>
2019-02-08 11:20:49 -0800

    Reorganized functions

--- server/sockets.js
+++ server/sockets.js
@@ -1,42 +1,9 @@
 import socket from 'socket.io';
 
-export class SocketConnection {
-  constructor(server, db) {
-    this.db = db;
-    this.io = socket(server);
-    this.setUpConnection();
-  }
-
-  get messages() {
-    return this.db
-      .collection('messages')
-      .find()
-      .toArray();
-  }
-
-  emitMessages = async (broadcast = false) => {
-    const emitter = broadcast ? this.io : this.client;
-    emitter.emit('messages', await this.messages);
-  };
-
-  handleNewMessage() {
-    this.client.on('newMessage', message => {
-      this.db
-        .collection('messages')
-        .insertOne({ text: message }, (err, result) => {
-          this.emitMessages(true);
-        });
-    });
-  }
-
-  setUpConnection = () => {
-    this.io.on('connection', client => {
-      this.client = client;
-      emitMessages();
-      handleNewMessage();
-    });
-  };
-}
+const emitMessages = async (db, emitter) => {
+  const messages = await getMessages(db);
+  emitter.emit('messages', messages);
+};
 
 const getMessages = db => {
   return db
@@ -45,19 +12,10 @@ const getMessages = db => {
     .toArray();
 };
 
-const emitMessages = async (db, client) => {
-  const messages = await getMessages(db);
-  client.emit('messages', messages);
-};
-
-const handleNewMessage = async (db, client, io) => {
-  client.on('newMessage', message => {
-    db.collection('messages').insertOne(
-      { text: message },
-      async (err, result) => {
-        io.emit('messages', await getMessages(db));
-      }
-    );
+const handleNewMessage = (db, client, io) => {
+  client.on('newMessage', async message => {
+    await db.collection('messages').insertOne({ text: message });
+    emitMessages(db, io);
   });
 };
 
@@ -68,9 +26,7 @@ const setUpConnection = (io, db) => {
   });
 };
 
-export default (server, db) => new SocketConnection(server, db);
-
-// export default (server, db) => {
-//   const io = socket(server);
-//   setUpConnection(io, db);
-// };
+export default (server, db) => {
+  const io = socket(server);
+  setUpConnection(io, db);
+};
